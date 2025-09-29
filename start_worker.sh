3a900bec3b24ddeb7997bb9a3b259b02a2abb798
Patricklolilol <patricklabarre364@gmail.com>
2025-09-29 10:38:37 +0200

    Update start_worker.sh

--- start_worker.sh
+++ start_worker.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -eu
 
-# Prefer REDIS_URL environment variable (Railway shared var name)
 : "${REDIS_URL:?Environment variable REDIS_URL must be set}"
 
-# Optional: log which redis url is being used (be careful if exposing secrets)
 echo "Starting rq worker with REDIS_URL=${REDIS_URL#*://*****@}"
 
-# Exec the worker so it receives signals correctly
 exec rq worker -u "$REDIS_URL" default
